ff268ab4fa7a60e39dd63cd55d3cf0111f5c849e
Christof Strack <christof.strack@softwareag.com>
2021-12-14 23:31:53 +0100

    removed unnedded code

--- mongo/mongo-setup.js
+++ mongo/mongo-setup.js
@@ -27,17 +27,9 @@ rsconf = {
 }
 
 rs.initiate(rsconf);
-let st= rs.status();
-
-// wait until replication set is created
-while (st['ok'] != 1) {
-    sleep(1000)
-    print("Waiting for replication set creation, status rs:", st['ok'])
-    st= rs.status();
-}
+let st = rs.status();
 print("Waiting (extra) for replication set creation, status rs:", st['ok'])
 sleep(5000)
-
 
 // create collections and index with ttl, so old measurements are deleted automatically
 keys = { datetime: 1 };
@@ -46,6 +38,5 @@ options = {
     expireAfterSeconds: parseInt(ttl)
 }
 print("Setting TTL for measurements to:", ttl)
-dbLocalDB2 = db.getSiblingDB('localDB')
-dbLocalDB2.measurement.createIndex(keys, options);
-dbLocalDB2.createCollection('serie')
+db.measurement.createIndex(keys, options);
+db.createCollection('serie')
