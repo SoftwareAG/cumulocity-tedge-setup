20bc0dcc447ff6e6d264c1d9c6d7d7426ee9cc91
sagIoTPower <christof.strack@softwareag.com>
2022-03-16 18:07:34 +0100

    added init scripts for web ui

--- tedge/start.sh
+++ tedge/start.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-node server.js
+[ -f /app/tedge/tedge-ui-env ] && . /app/tedge/tedge-ui-env
+
+if [ -z "$MONGO_HOST" ] ;  then
+  echo "MONGO_HOST is not set, please set it in /app/tedge/tedge-ui" >&2
+  exit 1
+fi
+node /app/tedge/server.js
