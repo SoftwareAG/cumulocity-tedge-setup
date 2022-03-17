602ff3e6a18c36c8018807ccc406403b3d56e84b
sagIoTPower <christof.strack@softwareag.com>
2022-03-17 08:10:25 +0100

    removed file tedge-ui-env and created this in Dockerfile instead

--- tedge/start.sh
+++ tedge/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 [ -f /app/tedge/tedge-ui-env ] && . /app/tedge/tedge-ui-env
 
-if [ -z "$MONGO_HOST" ] ;  then
-  echo "MONGO_HOST is not set, please set it in /app/tedge/tedge-ui" >&2
+if [ -z "$MONGO_HOST" ] &&   [ -z "$MONGO_PORT" ];  then
+  echo "MONGO_HOST or MONGO_PORT are not set, please set it in /app/tedge/tedge-ui" >&2
   exit 1
 fi
 node /app/tedge/server.js
